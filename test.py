dd31747044c509c4b4ccfff2d9e99f8db11447f7
Aniket <aniket.tameta@gmail.com>
2025-09-11 11:46:07 +0530

    n8n-test

--- test.py
+++ test.py
@@ -1,4 +1,41 @@
-a = 10
-b=20
+import math
+import os
+import sys
 
-print(a+b)
+def process(x):
+    return x * 2
+
+def do_something():
+    a = 10
+    b = 20
+    c = 30
+    d = []
+    for i in range(10):
+        if i % 2 == 0:
+            d.append(i * a)
+        else:
+            d.append(i * b)
+    for i in range(10):
+        if i % 2 == 0:
+            print(i * a)
+        else:
+            print(i * b)
+    return d
+
+def calc_area(r):
+    return 3.14 * r * r
+
+def main():
+    result = do_something()
+    print("Done processing")
+    for i in result:
+        if i > 100:
+            print("Large value:", i)
+        else:
+            print("Small value:", i)
+    try:
+        val = int("abc")
+    except:
+        print("Something went wrong")
+
+main()
